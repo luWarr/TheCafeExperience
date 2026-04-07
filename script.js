0e08f81b3acab6ea397a060f1cfa6f18051cf2c7
Lucy Warren <warreluc@sheridancollege.ca>
2026-04-07 11:58:49 -0400

    AHHH

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ console.log('Fetching CSV from:', CSV_URL);
 })();
 
 // set page background to images/pimping.jpeg
-document.body.style.backgroundImage = "url('images/wood.jpeg')";
+document.body.style.backgroundImage = "url('images/pimping.jpeg')";
 document.body.style.backgroundSize = 'cover';
 document.body.style.backgroundRepeat = 'no-repeat';
 document.body.style.backgroundPosition = 'center';
